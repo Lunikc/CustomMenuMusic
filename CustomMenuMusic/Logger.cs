76ce5d6da25c92c0a7b6ff9f0cbbe7071fca4d93
Lunikc <bissol.kenny@gmail.com>
2019-04-10 18:35:44 -1000

    Added logger

---
+++ CustomMenuMusic/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomMenuMusic.Misc
+{
+    static class Logger
+    {
+        public static void Log(object data)
+        {
+            Console.WriteLine($"[Custom Menu Music] {data}");
+        }
+
+        public static void Debug(object data)
+        {
+#if DEBUG
+            Console.WriteLine($"[Custom Menu Music // DEBUG] {data}");
+#endif
+        }
+    }
+}
